9a10d252f910e6c56985c620d810e82202652522
matan-shuster <shusmatan@gmail.com>
2022-05-26 19:43:42 +0300

    test

---
+++ src/ex1/script.js
@@ -0,0 +1,64 @@
+// trigger fork
+let count = 0;
+
+function addTodo(){
+    const todo = document.getElementById("newTodo").value;
+    if(todo.length === 0){
+        alert("Please enter a todo task")
+    }
+    else {
+        const todoList = document.getElementById("todoList");
+        const li = document.createElement("li");
+        li.id = 'todo' + count;
+        li.innerHTML = `<input type='checkbox' name='selectTodo' class= pl value="${todo}">`;
+        li.innerHTML += todo;
+        todoList.appendChild(li);
+        document.getElementById("newTodo").value = "";
+        count++;
+        console.log("is this checked" + li.checked);
+    }
+}
+document.getElementById("addTodo").addEventListener("click", addTodo);
+
+
+let history = [];
+function deleteTodo(){
+    let todos = document.querySelectorAll('.pl:checked');
+    todos.forEach(element => {
+            history.push(element.parentElement);
+            element.parentElement.remove();
+    })
+    console.log(document.getElementById("todoList").childNodes);
+}
+
+document.getElementById("delete").addEventListener("click",deleteTodo);
+
+
+// Event listeners for keypress
+window.addEventListener("keyup",e =>{
+    const todoList = document.getElementById("todoList");
+    //enable ctrl+z for adding delete items
+    if(e.ctrlKey && e.key === 'z'){
+        if(history.length === 0){
+            alert("History is currently empty")
+        }
+        else{
+            todoList.appendChild(history.pop());
+        }
+    }
+    //enable submitting todo via the enter key
+    if(e.key==='Enter'){
+        addTodo();
+    }
+    //enable deleting todo via the delete key
+    if(e.key === "Delete"){
+        deleteTodo();
+    }
+})
+
+document.addEventListener('click', function(e){
+    const target = e.target;
+    if(target.tagName === 'LI'){
+        alert("You've selected the following task: " + target.innerText);
+    }
+});
--- src/ex2/main.js
+++ src/ex2/main.js
@@ -1,4 +1,79 @@
+
+import ItemManager from './functions/ItemManager.js';
 // Implement the `Main` class here
+class Main{
+
+    init() {
+        const itemManager = new ItemManager(document.getElementById("todoList"));
+
+        function renderTodoList() {
+            document.getElementById("todoList").innerHTML = "";
+            const todoList = itemManager.getTodoList();
+            todoList.forEach(element => {
+                const li = document.createElement("li");
+                li.id = 'todo' + todoList.indexOf(element);
+                li.innerHTML = `<input type='checkbox' name='selectTodo' class= pl id="checkbox" value="${element}"> ${element} </input>`;
+                const deleteButton = document.createElement("img");
+                deleteButton.src = "./images/delete_icon.svg";
+                deleteButton.className = "deleteButton";
+                deleteButton.addEventListener("click", () => {
+                    deleteTodo(element);
+                });
+                li.appendChild(deleteButton);
+                document.getElementById("todoList").appendChild(li);
+
+            })
+
+        }
+        function getHistory() {
+            itemManager.getHistory();
+            renderTodoList();
+        }
+
+        // need to fix this
+        // function toggleDeleteButton() {
+        //    if(document.querySelectorAll('.pl:checked').length > 0){
+        //        document.getElementById("deleteTodo").hidden = false;
+        //        return
+        //    }
+        //    document.getElementById("deleteTodo").hidden = true;
+        //
+        // }
+        window.addEventListener("keyup", e => {
+            if(e.target.id !== "newTodo"){
+            if (e.ctrlKey && e.key === 'z') {
+                getHistory();
+            }
+        }})
+
+        async function addTodo() {
+            const todo = document.getElementById("newTodo").value;
+            document.getElementById("newTodo").value = "";
+            await itemManager.addTodo(todo);
+            renderTodoList();
+
+        }
+
+        document.getElementById("addTodo").addEventListener("click", addTodo);
+
+
+        function deleteSelected() {
+            let todos = document.querySelectorAll('.pl:checked');
+            todos.forEach(element => {
+                itemManager.deleteTodo(element.value);
+            });
+            renderTodoList();
+        }
+
+        function deleteTodo(todo) {
+            itemManager.deleteTodo(todo);
+            renderTodoList();
+        }
+
+        document.getElementById("deleteTodo").addEventListener("click", deleteSelected);
+
+    }
+}
 
 
 const main = new Main();
@@ -7,4 +82,5 @@ document.addEventListener("DOMContentLoaded", function () {
     // you should create an `init` method in your class
     // the method should add the event listener to your "add" button
     main.init();
+
 });
